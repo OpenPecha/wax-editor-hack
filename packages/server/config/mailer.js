2dad60f54f9311befdb2c4dfcac861094a809ebb
Giannis kopanas <jkopanas@gmail.com>
2023-12-02 18:58:29 +0200

    fix(server): mailer

--- packages/server/config/mailer.js
+++ packages/server/config/mailer.js
@@ -1,7 +1,8 @@
-const { MAILER_HOSTNAME, MAILER_USER, MAILER_PASSWORD } = process.env
+const { MAILER_HOSTNAME, MAILER_USER, MAILER_PASSWORD, MAILER_PORT } = process.env
 module.exports = {
   transport: {
     host: MAILER_HOSTNAME,
+    PORT: MAILER_PORT,
     auth: {
       user: MAILER_USER,
       pass: MAILER_PASSWORD,
